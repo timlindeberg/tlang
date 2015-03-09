5de92fb4d58df313d4bbaa0adb69e823efdbc7bc
Ludvig Axelsson <ludvigaxelsson@gmail.com>
2015-03-09 13:26:03 +0100

    Add test skeleton

--- build.sbt
+++ build.sbt
@@ -5,3 +5,12 @@ version := "1.0"
 scalaVersion := "2.11.4"
 
 scalacOptions ++= Seq("-deprecation", "-unchecked")
+
+libraryDependencies ++= Seq(
+	"org.scalatest" %% "scalatest" % "2.2.4" % "test"
+)
+
+// src/main/scala only
+unmanagedSourceDirectories in Compile := (scalaSource in Compile).value :: Nil
+// src/test/scala only
+unmanagedSourceDirectories in Test := (scalaSource in Test).value :: Nil
---
+++ src/test/scala/koolc/lexer/LexerSpec.scala
@@ -0,0 +1,23 @@
+package koolc.lexer
+
+import org.scalatest._
+import java.io.File
+
+import koolc.utils._
+import scala.collection.mutable.ArrayBuffer
+
+class LexerSpec extends FlatSpec with Matchers {
+
+  it should "lex kool programs" in {
+    val it = useLexer("valid-program-1.kool")
+    var tokens = new ArrayBuffer[Token]()
+    while (it.hasNext) tokens += it.next
+    
+    
+  }
+
+  def useLexer(fileName: String): Iterator[Token] = {
+    val ctx = new Context(reporter = new koolc.utils.Reporter, file = new File("./src/test/resources/" + fileName), outDir = None)
+    Lexer.run(ctx)(ctx.file)
+  }
+}
