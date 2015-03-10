6524750a0beca7722e664140dc5e956c49de702a
Ludvig Axelsson <ludvigaxelsson@gmail.com>
2015-03-10 15:54:26 +0100

    Add test case

--- src/test/scala/koolc/lexer/LexerSpec.scala
+++ src/test/scala/koolc/lexer/LexerSpec.scala
@@ -5,23 +5,31 @@ import java.io.File
 
 import koolc.utils._
 import scala.collection.mutable.ArrayBuffer
+import scala.io.Source
 
 class LexerSpec extends FlatSpec with Matchers {
+  val testResource = "./src/test/resources/"
 
-  it should "lex kool programs" in {
-    val it = useLexer("valid-program-1.kool")
-    
-    var tokens = new ArrayBuffer[Token]()
-    while (it.hasNext) tokens += it.next
-    println("Got tokens")
-    for(t <- tokens) {
-      print(t+"("+t.line+":"+t.col+") ")
-    }
-    
-  }
-
+  it should "lex valid program 1" in testFile("valid-program-1")
+  it should "lex valid program 2" in testFile("valid-program-2")
+  
   def useLexer(fileName: String): Iterator[Token] = {
-    val ctx = new Context(reporter = new koolc.utils.Reporter, file = new File("./src/test/resources/" + fileName), outDir = None)
+    val ctx = new Context(reporter = new koolc.utils.Reporter, file = new File(fileName), outDir = None)
     Lexer.run(ctx)(ctx.file)
   }
+
+  def testFile(file: String) {
+    val it = useLexer(testResource + file + ".kool")
+    val sol = readSolution(testResource + file + "-solution.kool")
+    (it.length == sol.length) && it.zip(sol).forall(x => printToken(x._1) == x._2)
+  }
+
+  def readSolution(fileName: String): Iterator[String] = {
+    Source.fromFile(fileName).getLines()
+  }
+
+  def printToken(token: Token): String = {
+    "(" + token.line + ":" + token.col + ")"
+  }
+
 }
