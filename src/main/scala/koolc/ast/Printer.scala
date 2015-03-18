a32fbe644d2c99ef9af21c0c83bfb8da2e015124
timlindeberg <tim.lindeberg@gmail.com>
2015-03-18 10:26:28 +0100

    Fixed bug with commas and pretty printer now prints prettily

--- src/main/scala/koolc/ast/Printer.scala
+++ src/main/scala/koolc/ast/Printer.scala
@@ -5,16 +5,32 @@ import Trees._
 
 object Printer {
 
+  var indent : Int = 0
+  
+  def l: String = {
+   indent += 1
+   "{" + n
+  }
+  
+  def r: String = {
+    indent -= 1
+    n + "}" 
+  }
+  
+  def n: String = {
+    "\n" + " " * (2 * indent)
+  }
+  
   def apply(t: Tree): String = t match {
     case Program(main, classes) => apply(main) + all(classes)
-    case MainObject(id, stats)  => "object " + id.value + " { def main () : Unit = { " + all(stats) + "}}"
+    case MainObject(id, stats)  => "object " + id.value + " " + l + "def main () : Unit = " + l + all(stats) + r + r
     case ClassDecl(id, parent, vars, methods) =>
-      "class " + id.value + optional(parent, " extends " + _.asInstanceOf[Identifier].value) +
-        " { " + all(vars) + all(methods) + "}"
-    case VarDecl(tpe, id) => "var " + id.value + " : " + apply(tpe) + " ;"
+      n + n + "class " + id.value + optional(parent, " extends " + _.asInstanceOf[Identifier].value) +
+        " " + l + all(vars) + all(methods) + "" + r
+    case VarDecl(tpe, id) => "var " + id.value + " : " + apply(tpe) + ";" + n
     case MethodDecl(retType, id, args, vars, stats, retExpr) =>
-      "def " + id.value + " ( " + commaList(args) + " ): " + apply(retType) +
-        " = { " + all(vars) + all(stats) + " return " + apply(retExpr) + " ; }"
+      "def " + id.value + " ( " + commaList(args) + " ) : " + apply(retType) +
+        " = " + l + all(vars) + all(stats) + "return " + apply(retExpr) + "; " + r + n
     case Formal(tpe, id)              => id.value + ": " + apply(tpe)
     // Types
     case IntType()                    => "Int"
@@ -22,12 +38,12 @@ object Printer {
     case BooleanType()                => "Bool"
     case StringType()                 => "String"
     // Statements
-    case Block(stats)                 => "{ " + all(stats) + " }"
-    case If(expr, thn, els)           => "if(" + apply(expr) + ")" + apply(thn) + optional(els, "else " + apply(_))
-    case While(expr, stat)            => "while(" + apply(expr) + ")" + apply(stat)
-    case Println(expr)                => "println(" + apply(expr) + ");"
-    case Assign(id, expr)             => id.value + " = " + apply(expr) + ";"
-    case ArrayAssign(id, index, expr) => id.value + "[" + apply(index) + "] = " + apply(expr) + ";"
+    case Block(stats)                 => l + all(stats) + r
+    case If(expr, thn, els)           => "if(" + apply(expr) + ")" + apply(thn) + optional(els, "else " + apply(_)) + n
+    case While(expr, stat)            => "while(" + apply(expr) + ")" + apply(stat) + n
+    case Println(expr)                => "println(" + apply(expr) + "); " + n
+    case Assign(id, expr)             => id.value + " = " + apply(expr) + "; " + n
+    case ArrayAssign(id, index, expr) => id.value + "[" + apply(index) + "] = " + apply(expr) + ";" + n
     // Expressions
     case And(lhs, rhs)                => apply(lhs) + " && " + apply(rhs)
     case Or(lhs, rhs)                 => apply(lhs) + " || " + apply(rhs)
@@ -55,7 +71,7 @@ object Printer {
   
   private def commaList(list: List[Tree]): String = {
     val s = list.foldLeft("")(_ + apply(_) + ", ")
-    s.substring(0, s.length - 1)
+    s.substring(0, s.length - 2) // remove last comma
   }
 
   private def all(list: List[Tree], start: String = ""): String = list.foldLeft(start)(_ + apply(_))
