a2aff3b71cce6780d880c169d0244f83f1be79c0
Tim Lindeberg <tim.lindeberg@gmail.com>
2018-02-10 16:27:27 +0100

    Initial commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import './App.css';
+import './semantic/dist/semantic.min.css';
+import HomepageLayout from './HomepageLayout';
+
+class App extends React.Component {
+
+    render() {
+        return <HomepageLayout/>;
+    }
+
+}
+
+export default App;
