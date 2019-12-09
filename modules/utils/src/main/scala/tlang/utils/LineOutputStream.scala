f422a94cb896e4b2ee78e2470f075beaf95cc0f1
Tim Lindeberg <tim.lindeberg@gmail.com>
2019-12-09 12:08:18 +0100

    Optimized LineOuptutStream

--- modules/utils/src/main/scala/tlang/utils/LineOutputStream.scala
+++ modules/utils/src/main/scala/tlang/utils/LineOutputStream.scala
@@ -2,9 +2,11 @@ package tlang
 package utils
 
 import java.io.{OutputStream, PrintStream}
+import java.nio.ByteBuffer
 import java.nio.charset.StandardCharsets
 
 object LineOutputStream {
+
   var OriginalOut: Option[PrintStream] = None
   val ThreadLocalStream: ThreadLocal[Option[OutputStream]] = ThreadLocal.withInitial(() => None)
 
@@ -24,39 +26,39 @@ object LineOutputStream {
 class LineOutputStream(originalOut: OutputStream, printLine: (String, Int) => Unit) extends PrintStream(originalOut) {
 
   private var lineNumber = 1
-  private val sb = new StringBuilder()
+  private val bytes: ByteBuffer = ByteBuffer.allocate(16384)
 
-  override def write(b: Array[Byte]): Unit = writeString(new String(b, StandardCharsets.UTF_8))
-  override def write(buf: Array[Byte], off: Int, len: Int): Unit = writeString(new String(buf, off, len, StandardCharsets.UTF_8))
+  override def write(b: Array[Byte]): Unit = write(b, 0, b.length)
+  override def write(buf: Array[Byte], off: Int, len: Int): Unit = {
+    var index = 0
+    while (index < len) {
+      val i = off + index
+      if (buf(i) == '\n') {
+        writeLine()
+      } else if (i < buf.length - 1 && buf(i) == '\r' && buf(i + 1) == '\n') {
+        writeLine()
+        index += 1
+      } else {
+        bytes.put(buf(i))
+      }
+      index += 1
+    }
+  }
 
   override def write(b: Int): Unit = {
     if (b == '\n') {
       writeLine()
     } else {
-      sb.append(b.asInstanceOf[Char])
+      bytes.put(b.asInstanceOf[Byte])
     }
   }
 
   override def flush(): Unit = {}
 
-  private def writeString(s: String): Unit = {
-    var i = 0
-    while (i < s.length) {
-      if (s(i) == '\n') {
-        writeLine()
-      } else if (i < s.length - 1 && s(i) == '\r' && s(i + 1) == '\n') {
-        writeLine()
-        i += 1
-      } else {
-        sb.append(s(i))
-      }
-      i += 1
-    }
-  }
-
   private def writeLine(): Unit = {
-    printLine(sb.toString, lineNumber)
-    sb.clear()
+    val s = new String(bytes.array(), 0, bytes.position(), StandardCharsets.UTF_8)
+    printLine(s, lineNumber)
+    bytes.clear()
     lineNumber += 1
   }
 }
--- modules/utils/src/main/scala/tlang/utils/StreamingProgramExecutor.scala
+++
@@ -1,2 +0,0 @@
-package tlang
-package utils
