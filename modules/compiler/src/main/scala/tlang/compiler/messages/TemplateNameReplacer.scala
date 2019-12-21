deb15b660fd4129be6106f0cf6b866e17a5baee9
Tim Lindeberg <tim.lindeberg@gmail.com>
2019-12-21 10:55:00 +0100

    TemplateNameReplacer can now replace multiple template names within a string

--- modules/compiler/src/main/scala/tlang/compiler/messages/TemplateNameReplacer.scala
+++ modules/compiler/src/main/scala/tlang/compiler/messages/TemplateNameReplacer.scala
@@ -4,39 +4,33 @@ package messages
 
 import scala.util.parsing.combinator.RegexParsers
 
-// Can only replace one template string per string which is fine for it's use case
 object TemplateNameReplacer extends RegexParsers {
 
   import tlang.compiler.modification.Templating._
 
-  def apply(s: String): String = {
-    val first = s.indexOf('-')
-    if (first == -1)
-      return s
-
-    val last = s.lastIndexOf('-')
-    if (last == -1)
-      return s
-
-    val preFix = s.substring(0, first)
-    val middle = s.substring(first, last + 1)
-    val postFix = s.substring(last + 1, s.length)
-
-    parseAll(template, middle) match {
-      case Success(res, _) => preFix + res + postFix
-      case NoSuccess(_, _) => s
-      case Failure(_, _)   => s
+  def apply(content: String): String = {
+    parseAll(full, content) match {
+      case Success(res, _) => res
+      case NoSuccess(_, _) => content
+      case Failure(_, _)   => content
     }
   }
 
   // Grammar
 
-  private def word: Parser[String] =
-    """[a-zA-Z\d_]+""".r ^^ {
-      _.toString
-    }
+  private def full: Parser[String] = textAndTemplate.* ^^ { values => values.mkString }
 
-  private def typeList: Parser[String] = ((Seperator ~ (word | template)) +) ^^ (list => list.map(_._2).mkString(", "))
+  private def textAndTemplate: Parser[String] = (anyText.? ~ template ~ anyText.?) ^^ { case before ~ replacedTemplate ~ after =>
+    before.getOrElse("") + replacedTemplate + after.getOrElse("")
+  }
+
+  private def anyText: Parser[String] = """([^\n\-]+)""".r ^^ { _.toString }
+
+  private def word: Parser[String] = """[a-zA-Z\d_]+""".r ^^ { _.toString }
+
+  private def typeList: Parser[String] = ((Seperator ~ (word | template)).+) ^^ { list =>
+    list.map(_._2).mkString(", ")
+  }
 
   private def template: Parser[String] = StartEnd ~ word ~ typeList ~ StartEnd ^^ {
     case _ ~ word ~ args ~ _ => s"$word<$args>"
--- modules/compiler/src/test/scala/tlang/compiler/messages/TemplateNameReplacerSpec.scala
+++ modules/compiler/src/test/scala/tlang/compiler/messages/TemplateNameReplacerSpec.scala
@@ -7,11 +7,15 @@ import tlang.testutils.UnitSpec
 class TemplateNameReplacerSpec extends UnitSpec {
 
   it should "replace template names" in {
-    TemplateNameReplacer("-A$T-") shouldBe "A<T>"
-    TemplateNameReplacer("-A$T1$T2$T3-") shouldBe "A<T1, T2, T3>"
-    TemplateNameReplacer("This is class -A$T1$T2$T3-, very cool") shouldBe "This is class A<T1, T2, T3>, very cool"
-    TemplateNameReplacer("A::B::-C$T1$T2-") shouldBe "A::B::C<T1, T2>"
-    TemplateNameReplacer("-A$-B$T1$T2--") shouldBe "A<B<T1, T2>>"
-    TemplateNameReplacer("-A$-B$T1$-C$T2$T3--$T4$-D$T5$T6-$T7-") shouldBe "A<B<T1, C<T2, T3>>, T4, D<T5, T6>, T7>"
+    replace("-A$T-") shouldBe "A<T>"
+    replace("-A$T1$T2$T3-") shouldBe "A<T1, T2, T3>"
+    replace("This is class -A$T1$T2$T3-, very cool") shouldBe "This is class A<T1, T2, T3>, very cool"
+    replace("A::B::-C$T1$T2-") shouldBe "A::B::C<T1, T2>"
+    replace("-A$-B$T1$T2--") shouldBe "A<B<T1, T2>>"
+    replace("-A$-B$T1$-C$T2$T3--$T4$-D$T5$T6-$T7-") shouldBe "A<B<T1, C<T2, T3>>, T4, D<T5, T6>, T7>"
+    replace("-A$-B$T1$-C$T2$T3--$T4$-D$T5$T6-$T7-") shouldBe "A<B<T1, C<T2, T3>>, T4, D<T5, T6>, T7>"
+    replace("TryTraverse(-Vec2$Int-, -A$-B$T1$T2--, Object).") shouldBe "TryTraverse(Vec2<Int>, A<B<T1, T2>>, Object)."
   }
+
+  private def replace(s: String): String = TemplateNameReplacer(s)
 }
